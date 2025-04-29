45b146eb9a2f8c4bc63126c77a2f0f4b64b208d1
DELL INSPIRON 5425 <DELL INSPIRON 5425@DESKTOP-RGQ18EV>
2025-04-30 00:00:02 +0700

    nhung

---
+++ baocao/dangky.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace baocao
+{
+    partial class dangky
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btndangky = new System.Windows.Forms.Button();
+            this.linkdangnhap = new System.Windows.Forms.LinkLabel();
+            this.txtmail = new System.Windows.Forms.TextBox();
+            this.txttendn = new System.Windows.Forms.TextBox();
+            this.txtmk = new System.Windows.Forms.TextBox();
+            this.txtnhaplaimk = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(275, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Đăng ký";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(229, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Email:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(128, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(181, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tên đăng nhập:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(194, 190);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 29);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Mật khẩu:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(99, 229);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(210, 29);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Nhập lại mật khẩu:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(139, 285);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(577, 29);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Nếu đã có tài khoản, hãy                      tại đây                 ";
+            // 
+            // btndangky
+            // 
+            this.btndangky.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btndangky.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndangky.Location = new System.Drawing.Point(283, 347);
+            this.btndangky.Name = "btndangky";
+            this.btndangky.Size = new System.Drawing.Size(165, 46);
+            this.btndangky.TabIndex = 6;
+            this.btndangky.Text = "Đăng ký";
+            this.btndangky.UseVisualStyleBackColor = false;
+            this.btndangky.Click += new System.EventHandler(this.btndangky_Click);
+            // 
+            // linkdangnhap
+            // 
+            this.linkdangnhap.AutoSize = true;
+            this.linkdangnhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkdangnhap.Location = new System.Drawing.Point(409, 288);
+            this.linkdangnhap.Name = "linkdangnhap";
+            this.linkdangnhap.Size = new System.Drawing.Size(118, 26);
+            this.linkdangnhap.TabIndex = 7;
+            this.linkdangnhap.TabStop = true;
+            this.linkdangnhap.Text = "Đăng nhập";
+            this.linkdangnhap.Click += new System.EventHandler(this.linkdangnhap_Click);
+            // 
+            // txtmail
+            // 
+            this.txtmail.Location = new System.Drawing.Point(386, 87);
+            this.txtmail.Name = "txtmail";
+            this.txtmail.Size = new System.Drawing.Size(221, 26);
+            this.txtmail.TabIndex = 8;
+            // 
+            // txttendn
+            // 
+            this.txttendn.Location = new System.Drawing.Point(386, 139);
+            this.txttendn.Name = "txttendn";
+            this.txttendn.Size = new System.Drawing.Size(221, 26);
+            this.txttendn.TabIndex = 9;
+            // 
+            // txtmk
+            // 
+            this.txtmk.Location = new System.Drawing.Point(386, 190);
+            this.txtmk.Name = "txtmk";
+            this.txtmk.Size = new System.Drawing.Size(221, 26);
+            this.txtmk.TabIndex = 10;
+            // 
+            // txtnhaplaimk
+            // 
+            this.txtnhaplaimk.Location = new System.Drawing.Point(386, 229);
+            this.txtnhaplaimk.Name = "txtnhaplaimk";
+            this.txtnhaplaimk.Size = new System.Drawing.Size(221, 26);
+            this.txtnhaplaimk.TabIndex = 11;
+            // 
+            // dangky
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtnhaplaimk);
+            this.Controls.Add(this.txtmk);
+            this.Controls.Add(this.txttendn);
+            this.Controls.Add(this.txtmail);
+            this.Controls.Add(this.linkdangnhap);
+            this.Controls.Add(this.btndangky);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "dangky";
+            this.Text = "dangky";
+            this.Load += new System.EventHandler(this.dangky_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btndangky;
+        private System.Windows.Forms.LinkLabel linkdangnhap;
+        private System.Windows.Forms.TextBox txtmail;
+        private System.Windows.Forms.TextBox txttendn;
+        private System.Windows.Forms.TextBox txtmk;
+        private System.Windows.Forms.TextBox txtnhaplaimk;
+    }
+}
