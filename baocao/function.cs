44bbbd7b2672df1d76c35b1f70a0443d3944885e
Admin <Admin@DESKTOP-36UK9PH>
2025-05-09 10:35:54 +0700

    alo

--- baocao/function.cs
+++ baocao/function.cs
@@ -14,7 +14,7 @@ namespace baocao
     {
         public static SqlConnection conn;  //Khai báo đối tượng kết nối
         public static string ConnectionString =
-            "Data Source=DESKTOP-4UBA1EH\\SQLEXPRESS02;Initial Catalog=quanlicuahangquanao;Integrated Security=True;Encrypt=True;Encrypt=False";
+            "Data Source=DESKTOP-36UK9PH\\LOCALHOST;Initial Catalog=qlcuahangquanao;Integrated Security=True;TrustServerCertificate=True";
 
         public static void Connect()
         {
